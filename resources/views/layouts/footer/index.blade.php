50753be69733c8b3aa693c1762007f25ae0df05e
vũ nguyễn tiến <nguyentienvu4897@gmail.com>
2023-01-11 10:32:11 +0700

    fixed

--- resources/views/layouts/footer/index.blade.php
+++ resources/views/layouts/footer/index.blade.php
@@ -10,10 +10,8 @@
       @endforeach
    </div>
    <section class="section footer-section" id="section_948757949">
-      <div class="bg section-bg fill bg-fill bg-loaded bg-loaded" >
-      </div>
-      <div class="section-content relative">
-      <div class="row row-large"  id="row-1850732756">
+      <div class="section-content">
+      <div class="row row-large" >
          <div id="col-149956225" class="col medium-5 small-12 large-5"  >
                <div class="col-inner"  >
                   <p style="text-align: justify;
@@ -40,7 +38,7 @@
          <div id="col-1493538928" class="col medium-7 small-12 large-7"  >
                <div class="col-inner"  >
                   <div class="row row-small"  id="row-2108084258">
-                  <div id="col-712618223" class="medium-6 small-12 large-6"  >
+                  <div id="col-712618223" class="col medium-6 small-12 large-6"  >
                      <div class="col-inner"  >
                            <p style="text-align: justify;
                            background-color: #105caa;
@@ -53,7 +51,7 @@
                            </ul>
                      </div>
                   </div>
-                  <div id="col-1081915389" class="medium-6 small-12 large-6"  >
+                  <div id="col-1081915389" class="col medium-6 small-12 large-6"  >
                      <div class="col-inner"  >
                            <p style="text-align: justify;
                            background-color: #105caa;
