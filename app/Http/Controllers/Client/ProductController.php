a2d3e7352df6ed49fbb94c3b243b8b826608bc9d
vũ nguyễn tiến <nguyentienvu4897@gmail.com>
2023-01-11 15:38:22 +0700

    fixed

--- app/Http/Controllers/Client/ProductController.php
+++ app/Http/Controllers/Client/ProductController.php
@@ -239,7 +239,7 @@ class ProductController extends Controller
             },
         ])->where('slug',$slug)->first(['id','name','images','type_cate','category','sku','discount','price','content','size','description','slug','preserve','cate_slug','type_slug','species','thickness']);
         $data['news'] = Blog::where('status',1)->limit(8)->get(['id','title','image','description','created_at','slug']);
-        $data['productlq'] = Product::where(['cate_slug'=>$cate, 'status'=>1])->take(10)->get(['id','name','images','discount','price','slug','cate_slug','type_slug','description']);
+        $data['productlq'] = Product::where(['cate_slug'=>$cate, 'status'=>1])->take(10)->get(['id','name','images','discount','price','slug','cate_slug','type_slug','description','origin','hang_muc']);
         $viewoldpro = session()->get('viewoldpro', []);
 
         if(isset($viewoldpro[$slug])) {
